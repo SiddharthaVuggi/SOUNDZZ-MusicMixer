26798303648bef9d8a4258295a0e607be15223f0
SiddharthaVuggi <141249647+SiddharthaVuggi@users.noreply.github.com>
2023-08-17 09:29:40 +0530

    Add files via upload

---
+++ user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    dob:{
+        type:String
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
+
